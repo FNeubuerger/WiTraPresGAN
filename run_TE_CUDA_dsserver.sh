dad49bfa3372e4bf1cb0f30ac3b7ec9e9697866a
FNeubuerger <fneubuerger@protonmail.com>
2022-05-11 14:16:07 +0200

    added main for te. still needs fixing for hardcoded numbers

---
+++ run_TE_CUDA_dsserver.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+train=true
+export TZ="GMT-8"
+
+# Experiment variables
+exp="Tennessee_Eastman"
+
+# Iteration variables
+emb_epochs=5000
+sup_epochs=5000
+gan_epochs=100
+
+python main.py \
+--device            cuda \
+--exp               $exp \
+--is_train          $train \
+--seed              420133769 \
+--feat_pred_no      1 \
+--max_seq_len       10000 \
+--train_rate        0.5 \
+--emb_epochs        $emb_epochs \
+--sup_epochs        $sup_epochs \
+--gan_epochs        $gan_epochs \
+--batch_size        10000 \
+--hidden_dim        20 \
+--num_layers        3 \
+--dis_thresh        0.15 \
+--optimizer         adam \
+--learning_rate     1e-3 \
